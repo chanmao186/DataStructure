02d08c6b9e49dae2d3ecc0dfff8ea74567cf6043
chanmao186 <chanmao186@qq.com>
2019-11-27 00:09:06 +0800

    完善了小明种苹果续算法

--- code/CCF_CSP/201909.02.小明种苹果_续.cpp
+++ code/CCF_CSP/201909.02.小明种苹果_续.cpp
@@ -3,7 +3,8 @@ using namespace std;
 #define N 1000
 int flag[N] = { 0 };
 int main(void) {
-	int n, m, _g, g, Applesum, treeSum, temp, CurrentApples, RecordNum;
+	long Applesum;
+	int n, m, _g, g,  treeSum, temp, CurrentApples, RecordNum;
 	_g = g = Applesum = treeSum = 0;
 	cin >> n;
 	for (int i = 0, j; i < n; i++) {
@@ -21,30 +22,32 @@ int main(void) {
 				CurrentApples -= abs(temp);
 			}
 		}
+		//累积总果数
 		Applesum += CurrentApples;
-		flag[i] = RecordNum;
+		//记录当前树是否发生落果
+		flag[i] = RecordNum>0;
 		if (flag[i]) {
+			//记录落果的树的数量
 			treeSum++;
+			//当前连续落果的树
 			_g++;
 		}
 		else {
 			_g = 0;
 		}
+
 		if (_g == 3) {
 			g++;
 			_g = 2;
 		}
 	}
-	if (n > 3) {
+	if (n >= 3) {
 		if (flag[0] && flag[1] && flag[n - 1]) {
 			g++;
 		}
 		if (flag[0] && flag[n - 1] && flag[n - 2]) {
 			g++;
 		}
-	}
-	if (n == 3 && flag[0] && flag[1] && flag[2]) {
-		g++;
 	}
 	cout << Applesum << " " << treeSum << " " << g << endl;
 	return 0;
