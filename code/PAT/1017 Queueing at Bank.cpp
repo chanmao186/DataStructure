61d134dca230df91d4d08be156f65edd362fbba9
chanmao186 <chanmao186@qq.com>
2020-02-01 00:16:19 +0800

    完成 1017 Queueing at Bank

--- code/PAT/1017 Queueing at Bank.cpp
+++ code/PAT/1017 Queueing at Bank.cpp
@@ -1,9 +1,7 @@
 #include<iostream>
 #include<vector>
-#include<set>
 #include<algorithm>
 using namespace std;
-#define MaxK 100
 const int Start = 3600 * 8;
 const int End = 3600 * 17;
 struct Customer
@@ -19,7 +17,7 @@ struct Customer
 		done = ctime + Occupied;
 	}
 	bool operator<(Customer other)const {
-		return done < other.done;
+		return done == other.done ? Arrival < other.Arrival : done < other.done;
 	}
 };
 bool cmp(const Customer c1, const Customer c2) {
@@ -28,18 +26,13 @@ bool cmp(const Customer c1, const Customer c2) {
 int main() {
 	int n, k;
 	cin >> n >> k;
-	set<Customer> window;
-	int MaxOccupide = 60;
-	string arr;
+	vector<Customer> window;
 	int Occ;  //占用时间
 	int h, m, s, temp;
 	vector<Customer> cus;
-	vector<Customer> wait;
 	int WaitTime = 0;//	等待时间
 	for (int i = 0; i < n; i++) {
-		cin >> arr >> Occ;
-		sscanf_s(arr.data(), "%d:%d:%d", &h, &m, &s);
-
+		scanf_s("%d:%d:%d %d", &h, &m, &s, &Occ);
 		temp = h * 3600 + m * 60 + s;
 		if (temp > End) {
 			continue;
@@ -51,50 +44,43 @@ int main() {
 		cus.push_back(cc);
 	}
 
-	sort(cus.begin(), cus.end(),cmp);
-
-	for (vector<Customer>::iterator x = cus.begin(); x != cus.end(); x = cus.begin()) {
-		if (x->Arrival >= Start)break;
-		WaitTime += Start - x->Arrival - 1;
-		wait.push_back(*x);
-		cus.erase(cus.begin());
-	}
-
+	sort(cus.begin(), cus.end(), cmp);
+	n = cus.size();
 	int cTime = Start;
-	int _internal = 1;
-	while (wait.size() > 0 || cus.size() > 0)
+	while (cus.size() > 0)
 	{
-		WaitTime += wait.size() * _internal;
-		if (cus.size() > 0 && cus.front().Arrival == cTime) {
-			wait.push_back(cus.front());
-			cus.erase(cus.begin());
-		}
-
-		if (window.size() > 0 && window.begin()->done == cTime) {
+		while (window.size() > 0 && window.front().done <= cTime) {
 			window.erase(window.begin());
 		}
-
-		while (window.size() < k && wait.size() > 0)
-		{
-			wait.front().getDone(cTime);
-			window.insert(wait.front());
-			wait.erase(wait.begin());
+		while (cus.size() > 0 && cus.front().Arrival <= cTime && window.size() < k) {
+			cus.front().getDone(cTime);
+			WaitTime += cTime - cus.front().Arrival;//累加等待时间			
+			window.push_back(cus.front());
+			cus.erase(cus.begin());
 		}
-
-		if (window.size() == 0) {
-			_internal = cus.front().Arrival - cTime;
-			cTime = cus.front().Arrival;
-		}
-		else if(cus.size()==0){
-			_internal = window.begin()->done - cTime;
-			cTime = window.begin()->done - cTime;
+		if (window.size() > 0)
+			sort(window.begin(), window.end());
+		//更新当前时间为在下一个客户完成的时间
+		if (window.size() == k) {
+			cTime = window.begin()->done;
 		}
 		else {
-			_internal = max(window.begin()->done, cus.front().Arrival) - cTime;
-			cTime = max(window.begin()->done, cus.front().Arrival);
+			cTime = cus.front().Arrival;
 		}
 	}
-	double ave = WaitTime / 60.0;
+	double ave;
+	if (n > 0)
+		ave = WaitTime / 60.0 / n;
+	else
+		ave = 0;
 	printf("%.1f", ave);
 	return 0;
 }
+
+/*
+总结：
+本次是因为设计的算法上逻辑有问题，导致测试一直没有通过
+教训
+1.要多练孰能能生巧；
+2.换个算法
+*/
